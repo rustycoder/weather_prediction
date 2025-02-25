eacd55f65285a6e8ff3580c33da0729797ec9f8b
Prajun Adhikary <prajun.adhikary@gmail.com>
2025-02-25 11:52:09 +0545

    Login and Logout for Weather Admin

--- weather_admin/urls.py
+++ weather_admin/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login', views.login, name='weather_admin_login'),
-    path('forgot_password', views.forgot_password, name='weather_admin_forgot_password'),
-    path('register', views.register, name='weather_admin_register'),
-    path('dashboard', views.dashboard, name='weather_admin_dashboard'),
+    path('', views.admin_home, name='weather_admin_home'),
+    path('login', views.admin_login, name='weather_admin_login'),
+    path('forgot_password', views.admin_forgot_password, name='weather_admin_forgot_password'),
+    path('register', views.admin_register, name='weather_admin_register'),
+    path('logout', views.admin_logout, name='weather_admin_logout'),
 ]
--- weather_admin/views.py
+++ weather_admin/views.py
@@ -1,20 +1,47 @@
+from django.shortcuts import redirect, render
 from django.template import loader
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 
-# Create your views here.
-def login(request):
-    template = loader.get_template('login.html')
-    return HttpResponse(template.render())
+def admin_home(request):
+    if request.user.is_authenticated:
+        return render(request, 'home.html', {})
+    else:
+        return render(request, 'login.html', {})
 
-def forgot_password(request):
+def admin_login(request):
+    if request.user.is_authenticated:
+        messages.success(request, "You have been logged in.")
+        return render(request, 'home.html', {})
+    else:
+        if request.method == 'POST':
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(request=request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.success(request, "You have been logged in.")
+                return render(request, 'home.html', {})
+            else:
+                messages.success(request, "There was an error. Please, try again.")
+                return redirect('weather_admin_login')
+        else:
+            return render(request, 'login.html', {})
+
+def admin_forgot_password(request):
     template = loader.get_template('forgot_password.html')
     return HttpResponse(template.render())
 
-def register(request):
-    template = loader.get_template('register.html')
-    return HttpResponse(template.render())
+def admin_register(request):
+    if request.user.is_authenticated:
+        messages.success(request, "You have been logged in.")
+        return redirect('weather_admin_home')
+    else:
+        return render(request, 'register.html', {})
 
-def dashboard(request):
-    template = loader.get_template('dashboard.html')
-    return HttpResponse(template.render())
+def admin_logout(request):
+    logout(request)
+    messages.success(request, "You have logged out.")
+    return redirect('weather_admin_home')
