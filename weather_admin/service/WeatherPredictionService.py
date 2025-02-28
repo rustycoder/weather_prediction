973516b89ffc517b3e165d20ae18c3afa8e69e18
Prajun Adhikary <prajun.adhikary@gmail.com>
2025-03-01 00:25:06 +0545

    Prediction

--- weather_admin/service/WeatherPredictionService.py
+++ weather_admin/service/WeatherPredictionService.py
@@ -7,12 +7,15 @@ import os
 class WeatherPredictionService:
 
     def predict_weather_main(self, temperature, humidity, pressure):
-        logger.debug(static('/files/kathmandu_weather_data_model.pkl'))
-        model_file_path = os.path.join(os.path.dirname(__file__), 'static/files/kathmandu_weather_data_model.pkl') 
+        
+        model_file_path = os.path.join(os.path.dirname(__file__), '../static/files/kathmandu_weather_data_model.pkl') 
+        logger.debug(model_file_path)
 
         with open(model_file_path, 'rb') as m:
             kathmandu_weather_data_model = pickle.load(m)
-            
+
         X = np.array([[temperature,humidity,pressure]])
         y = kathmandu_weather_data_model.predict(X)
-        return y
+        logger.debug(y[0])
+        
+        return y[0]
