8860818f519b5f1288e947a838579c23732aecce
Prajun Adhikary <prajun.adhikary@gmail.com>
2025-03-04 07:06:52 +0545

    Requirement.txt file created

---
+++ .history/weather_admin/views_20250304063913.py
@@ -0,0 +1,311 @@
+from django.shortcuts import redirect, render
+from django.http import JsonResponse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.core.mail import send_mail
+from django.contrib.auth.models import User
+
+from weather_prediction.settings import EMAIL_HOST_USER
+
+from .service.OpenWeatherService import OpenWeatherService
+from .service.WeatherPredictionService import WeatherPredictionService
+from .forms import SignUpForm, ProfileForm
+from .models import Profile
+
+from loguru import logger
+
+import random
+
+
+def admin_dashboard(request):
+    if request.user.is_authenticated:
+        latitude = request.GET['latitude'] if request.method == 'GET' and 'latitude' in request.GET else 27.7172
+        longitude = request.GET['longitude'] if request.method == 'GET' and 'longitude' in request.GET else 85.3240
+
+        current_user = User.objects.get(username=request.user)
+        current_profile = Profile.objects.get(user=current_user)
+
+        open_weather_service = OpenWeatherService()
+        weather_data, error_message = open_weather_service.current_weather_data(latitude=latitude, longitude=longitude)
+        logger.debug(weather_data)
+
+        weather_prediction_service = WeatherPredictionService()
+        temperature = weather_data['main']['temp']
+        humidity = weather_data['main']['humidity']
+        pressure = weather_data['main']['pressure']
+        predicted_weather_main = weather_prediction_service.predict_weather_main(
+            temperature=temperature,
+            humidity=humidity,
+            pressure=pressure
+        )
+        logger.debug(predicted_weather_main)
+        
+        air_pollution_data, error_message = open_weather_service.current_air_pollution_data(latitude=latitude, longitude=longitude)
+        logger.debug(air_pollution_data)
+        
+        return render(request, 'dashboard.html', {'current_user':current_user, 'current_profile':current_profile, 'weather_data':weather_data, 'air_pollution_data':air_pollution_data, 'predicted_weather_main':predicted_weather_main})
+    else:
+        return render(request, 'login.html', {})
+
+
+def admin_search(request):
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            location = request.POST['location']
+            logger.debug(location)
+            if len(location)!=0:
+                logger.debug("Location found")
+                open_weather_service = OpenWeatherService()
+                weather_data, error_message = open_weather_service.current_weather_search(location=location)
+                logger.debug(weather_data)    
+                messages.success(request, f"Weather overview for {location}.")
+                return render(request, 'search.html', {'weather_data':weather_data, 'location':location})
+        return redirect('weather_admin_dashboard')
+    return render(request, 'login.html', {})
+
+
+def profile(request):
+    if request.user.is_authenticated:
+        current_user = User.objects.get(username=request.user)
+        current_profile = Profile.objects.get(user=current_user)
+        return render(request, 'profile.html', {'current_user':current_user, 'current_profile':current_profile})
+    else:
+        return render(request, 'login.html', {})
+
+
+
+def weather_realtime(request):
+    if request.user.is_authenticated:
+        open_weather_service = OpenWeatherService()
+        weather_data, error_message = open_weather_service.current_weather_data(27.7172, 85.3240)
+        logger.debug(weather_data)
+        return render(request, 'weather_realtime.html', {'weather_data':weather_data})
+    else:
+        return render(request, 'login.html', {})
+
+
+
+def air_pollution_realtime(request):
+    if request.user.is_authenticated:
+        open_weather_service = OpenWeatherService()
+        air_pollution_data, error_message = open_weather_service.current_air_pollution_data(27.7172, 85.3240)
+        logger.debug(air_pollution_data)
+        return render(request, 'air_pollution_realtime.html', {'air_pollution_data':air_pollution_data})
+    else:
+        return render(request, 'login.html', {})
+    
+
+
+def weather_forecast(request):
+    if request.user.is_authenticated:
+        open_weather_service = OpenWeatherService()
+        weather_forecast, error_message = open_weather_service.five_day_weather_forecast(27.7172, 85.3240)
+        logger.debug(weather_forecast)
+        return render(request, 'weather_forecast.html', {'weather_forecast':weather_forecast})
+    else:
+        return render(request, 'login.html', {})
+
+
+
+def air_pollution_forecast(request):
+    if request.user.is_authenticated:
+        open_weather_service = OpenWeatherService()
+        air_pollution_forecast, error_message = open_weather_service.forecast_air_pollution_data(27.7172, 85.3240)
+        logger.debug(air_pollution_forecast)
+        return render(request, 'air_pollution_forecast.html', {'air_pollution_forecast':air_pollution_forecast})
+    else:
+        return render(request, 'login.html', {})
+
+
+
+def weather_prediction(request):
+    if request.user.is_authenticated:
+        open_weather_service = OpenWeatherService()
+        weather_data, error_message = open_weather_service.current_weather_data(27.7172, 85.3240)
+
+        weather_prediction_service = WeatherPredictionService()
+        temperature = weather_data['main']['temp']
+        humidity = weather_data['main']['humidity']
+        pressure = weather_data['main']['pressure']
+        predicted_weather_main = weather_prediction_service.predict_weather_main(
+            temperature=temperature,
+            humidity=humidity,
+            pressure=pressure
+        )
+        
+        logger.debug(weather_data)
+        return render(request, 'weather_prediction.html', {'temperature':temperature, 'humidity':humidity, 'pressure':pressure, 'predicted_weather_main':predicted_weather_main})
+    else:
+        return render(request, 'login.html', {})
+''
+
+
+def admin_login(request):
+    if request.user.is_authenticated:
+        messages.success(request, "You have been logged in.")
+        return redirect('weather_admin_dashboard')
+    else:
+        if request.method == 'POST':
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(request=request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.success(request, "You have been logged in.")
+                return redirect('weather_admin_dashboard')
+            else:
+                messages.success(request, "Invalid username and password. Please try again!.")
+                return render(request, 'login.html', {})
+        else:
+            return render(request, 'login.html', {})
+
+
+
+def admin_register(request):
+    if request.user.is_authenticated:
+        messages.success(request, "You have been logged in.")
+        return redirect('weather_admin_dashboard')
+    else:
+        if request.method == 'POST':
+            form = SignUpForm(request.POST)
+            if form.is_valid():
+                form.save()
+                # Authenticate and login
+                username = form.cleaned_data['username']
+                password = form.cleaned_data['password1']
+                user = authenticate(username=username, password=password)
+                logger.debug(user)
+                login(request, user)
+                current_user = User.objects.get(username=user)
+                current_profile = Profile()
+                logger.debug(current_profile)
+                current_profile.otp = random.randint(1000000, 9999999)
+                current_profile.user = current_user
+                current_profile.save()
+                messages.success(request, "You Have Successfully Registered! Welcome!")
+                return redirect('weather_admin_dashboard')
+        else:
+            form = SignUpForm()
+            return render(request, 'register.html', {'form':form})
+    return render(request, 'register.html', {'form':form})
+
+
+
+def profile_update(request):
+    if request.user.is_authenticated:
+        current_user = User.objects.get(username=request.user)
+        current_profile = Profile.objects.get(user=current_user)
+        if request.method == 'GET':
+            form = ProfileForm(instance=current_profile)
+            return render(request, 'profile_update.html', {'form':form})
+        elif request.method == 'POST':
+            form = ProfileForm(request.POST)
+            if form.is_valid():
+                current_profile.phone = form.cleaned_data['phone']
+                current_profile.address = form.cleaned_data['address']
+                current_profile.city = form.cleaned_data['city']
+                current_profile.state = form.cleaned_data['state']
+                current_profile.zipcode = form.cleaned_data['zipcode']
+                current_profile.save()
+                messages.success(request, "You Have Successfully Updated Your Profile.")
+                return redirect('weather_admin_profile')
+            else:
+                return render(request, 'profile_update.html', {'form':form})
+    else:
+        return render(request, 'login.html', {})
+
+
+
+def admin_logout(request):
+    logout(request)
+    messages.success(request, "You have logged out.")
+    return redirect('weather_admin_login')
+
+
+
+def admin_forgot_password(request):
+    if request.user.is_authenticated:
+        messages.success(request, "You have been logged in.")
+        return redirect('weather_admin_dashboard')
+    else:
+        if request.method == 'POST':
+            email = request.POST['email']
+            if User.objects.filter(email=email).exists():
+                # TODO Send Email
+                user = User.objects.get(email=email)
+                profile = Profile.objects.get(user=user)
+                send_mail(
+                    "Reset your password : Weather Prediction Application",
+                    f"Hey {user}, want to reset password. Click on the link http://127.0.0.1:8000/weather_admin/reset_password?username={user}&otp={profile.otp}",
+                    EMAIL_HOST_USER,
+                    [email],
+                    fail_silently=False,
+                )
+                messages.success(request, f"Reset password link has been sent to your {email}.")
+                return render(request, 'forgot_password.html', {})
+            else:
+                messages.success(request, f"{email} does not exist.")
+                return render(request, 'forgot_password.html', {})
+        else:
+            return render(request, 'forgot_password.html', {})
+
+
+
+def admin_reset_password(request):
+    if request.user.is_authenticated:
+        messages.success(request, "You have been logged in.")
+        return redirect('weather_admin_dashboard')
+    else:
+        if request.method == 'GET':
+            username = request.GET['username']
+            otp = request.GET['otp']
+            return render(request, 'reset_password.html', {'username':username, 'otp':otp})
+        elif request.method == 'POST':
+            username = request.POST['username']
+            otp = request.POST['otp']
+            password1 = request.POST['password1']
+            password2 = request.POST['password2']
+            if password1 == password2:
+                if User.objects.filter(username=username).exists():
+                    db_user = User.objects.get(username=username)
+                    db_profile = Profile.objects.get(user=db_user)
+                    if db_profile.otp == otp:
+                        db_user.set_password(password1)
+                        db_user.save()
+                        messages.success(request, f"Password has been reset. Try to login with new password")
+                        return redirect('weather_admin_login')
+                    else:
+                        messages.success(request, f"Password do not match. Try again")
+                        return render(request, 'reset_password.html', {'username':username, 'otp':otp})
+            else:
+                messages.success(request, f"Password do not match. Try again")
+                return render(request, 'reset_password.html', {'username':username, 'otp':otp})
+        else:
+            return redirect('weather_admin_login')
+        
+
+def api_weather_overview(request):
+    latitude = request.GET.get('latitude')
+    longitude = request.GET.get('longitude')
+
+    open_weather_service = OpenWeatherService()
+    weather_data, error_message = open_weather_service.current_weather_data(latitude=latitude, longitude=longitude)
+    air_pollution_data, error_message = open_weather_service.current_air_pollution_data(27.7172, 85.3240)
+
+    weather_prediction_service = WeatherPredictionService()
+    temperature = weather_data['main']['temp']
+    humidity = weather_data['main']['humidity']
+    pressure = weather_data['main']['pressure']
+    predicted_weather_main = weather_prediction_service.predict_weather_main(
+        temperature=temperature,
+        humidity=humidity,
+        pressure=pressure
+    )
+    
+    return JsonResponse({
+        'latitude':latitude,
+        'longitude':longitude,
+        'weather':weather_data,
+        'air_pollution':air_pollution_data,
+        'prediction':predicted_weather_main
+    })
