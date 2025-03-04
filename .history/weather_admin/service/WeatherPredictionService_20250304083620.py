11faaca23d24479cad341944870ae761c535b9c4
Prajun Adhikary <prajun.adhikary@gmail.com>
2025-03-04 08:36:34 +0545

    Loguru removed

--- .history/weather_admin/service/WeatherPredictionService_20250304083620.py
+++ .history/weather_admin/service/WeatherPredictionService_20250304083620.py
@@ -8,11 +8,13 @@ class WeatherPredictionService:
     def predict_weather_main(self, temperature, humidity, pressure):
         
         model_file_path = os.path.join(os.path.dirname(__file__), '../static/files/kathmandu_weather_data_model.pkl') 
+        logger.debug(model_file_path)
 
         with open(model_file_path, 'rb') as m:
             kathmandu_weather_data_model = pickle.load(m)
 
         X = np.array([[temperature,humidity,pressure]])
         y = kathmandu_weather_data_model.predict(X)
+        logger.debug(y[0])
         
         return y[0]
